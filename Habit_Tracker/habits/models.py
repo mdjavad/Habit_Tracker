5e08c2060d00b76af59822fdc31b92968de9d217
mdjavad <javadmohd78@gmail.com>
2025-07-07 18:35:34 +0530

    added modules

--- Habit_Tracker/habits/models.py
+++ Habit_Tracker/habits/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
-# Create your models here.
+ class User(models.Model): 
+
+           User_id = models.IntegerField() 
+           name =  models.CharField(max_length=100) 
+           email   =  models.EmailField() 
+           date_of_birth = models.DateField() 
+           password = models.CharField(max_length=128) 
