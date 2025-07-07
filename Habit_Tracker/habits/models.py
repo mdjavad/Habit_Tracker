7d50edea37764b171ec285e2e0e509bc263551bf
mohammedismail0121 <ismail.mohammed90086@gmail.com>
2025-07-07 18:35:46 +0530

    added modules

--- Habit_Tracker/habits/models.py
+++ Habit_Tracker/habits/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
-# Create your models here.
+class habits(models.Model):
+    Habit_id = models.IntegerField()
+    User_id = models.IntegerField(models.ForeignKey("app.Model", verbose_name=_(""), on_delete=models.CASCADE))
+    Habit_name = models.CharField() 
+    description = models.CharField()
+    duration = models.DurationFieldField()
+    reminder = models.AutoField()
