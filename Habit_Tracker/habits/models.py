771340fe2a7fcc1b4b5e8570e79e183f2730900e
MohdOwais46 <owais68742@gmail.com>
2025-07-08 18:30:54 +0530

    added models by owais

--- Habit_Tracker/habits/models.py
+++ Habit_Tracker/habits/models.py
@@ -2,6 +2,6 @@ from django.db import models
 
 class HabitLog(models.Model):
     id = models.IntegerField()
-    habit_id = models.ForeignKey(Habit, on_delete=models.CASCADE)
+    habit_id = models.ForeignKey()
     date = models.DateField()
     status = models.CharField(max_length=50)
