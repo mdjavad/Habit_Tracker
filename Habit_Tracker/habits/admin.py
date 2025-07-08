bb5097d436f38a201d9d5ab2895d5c7c84e2bf6b
mdjavad <javadmohd78@gmail.com>
2025-07-08 20:14:57 +0530

    runserver completed

--- Habit_Tracker/habits/admin.py
+++ Habit_Tracker/habits/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import User, Habit, HabitLog
+
+admin.site.register(User)
+admin.site.register(Habit)
+admin.site.register(HabitLog)
--- Habit_Tracker/habits/models.py
+++ Habit_Tracker/habits/models.py
@@ -1,23 +1,28 @@
 from django.db import models
 
-
 class User(models.Model): 
+    name = models.CharField(max_length=100) 
+    email = models.EmailField()
+    date_of_birth = models.DateField() 
+    password = models.CharField(max_length=128)
 
-           User_id = models.IntegerField() 
-           name =  models.CharField(max_length=100) 
-           email   =  models.EmailField()
-           date_of_birth = models.DateField() 
-           password = models.CharField(max_length=128) 
-class habits(models.Model):
-    Habit_id = models.IntegerField()
-    User_id = models.IntegerField(models.ForeignKey("app.Model", verbose_name= (""), on_delete=models.CASCADE))
-    Habit_name = models.CharField() 
-    description = models.CharField()
-    duration = models.DurationFieldField()
-    reminder = models.AutoFill()
-    
-    class HabitLog(models.Model):
-    id = models.IntegerField()
-    habit_id = models.ForeignKey()
+    def __str__(self):
+        return self.name
+
+class Habit(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    name = models.CharField(max_length=100) 
+    description = models.TextField()
+    duration = models.DurationField()
+    reminder = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.name
+
+class HabitLog(models.Model):
+    habit = models.ForeignKey(Habit, on_delete=models.CASCADE)
     date = models.DateField()
     status = models.CharField(max_length=50)
+
+    def __str__(self):
+        return f"{self.habit.name} - {self.date}"
